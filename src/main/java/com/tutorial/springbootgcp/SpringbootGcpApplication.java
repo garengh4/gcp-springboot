83937f1f7ed5c2129df78066b33f1e1fedc47770
Garen <garengh4@gmail.com>
2023-04-05 12:53:08 -0400

    init commit 2

--- src/main/java/com/tutorial/springbootgcp/SpringbootGcpApplication.java
+++ src/main/java/com/tutorial/springbootgcp/SpringbootGcpApplication.java
@@ -2,10 +2,21 @@ package com.tutorial.springbootgcp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class SpringbootGcpApplication {
+public class SpringbootGcpApplication extends WebMvcConfigurerAdapter {
 
+
+
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootGcpApplication.class, args);
 	}
