cf67822133402b5d5d9cce88822576018616b7d7
Garen <garengh4@gmail.com>
2023-04-05 10:49:46 -0400

    init commit

---
+++ src/main/java/com/tutorial/springbootgcp/SpringbootGcpApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.springbootgcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/springbootgcp/SpringbootGcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.springbootgcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
